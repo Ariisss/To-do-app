1f3a43ee0e97d5056f747a4362f65b247b857d73
Prince Isaac Pantino <arispantino@gmail.com>
2024-09-16 18:47:39 +0800

    feat: created a toggle button for check/unchecking all tasks

---
+++ components/CheckUncheckAllButton.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { TouchableOpacity, StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { Colors } from '@/constants/Colors';
+
+interface CheckUncheckAllButtonProps {
+  onPress: () => void;
+  allCompleted: boolean;
+}
+
+export default function CheckUncheckAllButton({ onPress, allCompleted }: CheckUncheckAllButtonProps) {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Ionicons
+        name={allCompleted ? "checkbox" : "square"}
+        size={24}
+        color={"white"}
+      />
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    position: 'absolute',
+    right: 24,
+    bottom: 16,
+    backgroundColor: Colors.light.primary,
+    borderRadius: 30,
+    width: 70,
+    height: 70,
+    justifyContent: 'center',
+    alignItems: 'center',
+    elevation: 5,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+  },
+});
